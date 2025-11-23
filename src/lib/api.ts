31b649701e090977c9f2790eedcd090d42f80922
minhhung7 <hungm0434@gmail.com>
2025-11-23 07:06:59 +0700

    fix url BE

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,7 +4,7 @@ import { API_BASE_URL } from "@/constants/api";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
-  withCredentials: true, // keep if your backend uses cookies
+  withCredentials: false, // keep if your backend uses cookies
   headers: { "Content-Type": "application/json" },
 });
 
